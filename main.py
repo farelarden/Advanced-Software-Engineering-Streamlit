57b07bf76a8ec3037dec1848728cedde2c5aa422
Farel Arden <farelarden13@gmail.com>
2024-11-16 22:00:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ def main():
 
     df_geodat_plz   = pd.read_csv("datasets/geodata_berlin_plz.csv", sep=';')
     
-    df_lstat = pd.read_excel("datasets\downloaded_datasets\Ladesaeulenregister_SEP.xlsx", header=10)
+    df_lstat = pd.read_excel("datasets/downloaded_datasets/Ladesaeulenregister_SEP.xlsx", header=10)
     # df_lstat2       = pd.read_csv("datasets\downloaded_datasets\Ladesaeulenregister_SEP.xlsx")
     # gdf_lstat3      = pd.read_csv("datasets\downloaded_datasets\Ladesaeulenregister.csv")
     
-    df_residents    = pd.read_csv("datasets\downloaded_datasets\plz_einwohner.csv")
+    df_residents    = pd.read_csv("datasets/downloaded_datasets\plz_einwohner.csv")
     # pd.read_csv("datasets\downloaded_datasets\Ladesaeulenregister_SEP.xlsx") #I'm not sure
-    gdf_residents2  = pd.read_csv("datasets\geodata_berlin_dis.csv", sep=';')
+    gdf_residents2  = pd.read_csv("datasets/geodata_berlin_dis.csv", sep=';')
     
 # -----------------------------------------------------------------------------------------------------------------------
 
